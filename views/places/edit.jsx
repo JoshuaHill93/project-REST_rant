70c035b854639b02af7e01c9df35f9ede110fb42
joshua_hill <joshuah923@gmail.comc:/Users/joshu/OneDrive/Desktop/hacker/CSS-The-Hacker-Times-Part-1/CSS-The-Hacker-Times-Part-1/index.html>
2023-11-19 11:46:01 -0800

    Added the edit functionality

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,6 +1,8 @@
 const React = require('react')
 const Def = require('../default.jsx')
 
+
+
 function edit_form () {
     return (
         <Def>
@@ -15,22 +17,39 @@ function edit_form () {
 
 
 
+
+<form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+  <div className="form-group">
+      <label htmlFor="name">Place Name</label>
+      <input 
+        className="form-control" 
+        id="name" 
+        name="name" 
+        value={data.place.name} 
+      required />
+  </div>
+  ...
+</form>
+
+
+
+
+
+
+
+
 router.get('/:id/edit', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else {
-      res.render('places/edit', { place: places[id] })
-    }
-  })
-  
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
 
 
-  
-
-  
 module.exports = edit_form
