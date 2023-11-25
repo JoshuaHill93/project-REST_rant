2ed1196386944835cfe2eed7becf2bedfc363bc4
joshua_hill <joshuah923@gmail.comc:/Users/joshu/OneDrive/Desktop/hacker/CSS-The-Hacker-Times-Part-1/CSS-The-Hacker-Times-Part-1/index.html>
2023-11-24 20:53:28 -0800

    fixing edit page

--- controllers/places.js
+++ controllers/places.js
@@ -1,147 +1,81 @@
-const router = require('express').Router()
-const db = require('../models')
+const router = require("express").Router();
+const db = require("../models");
 
-router.get('/', (req, res) => {
-    db.Place.find()
+//INDEX
+router.get("/", (req, res) => {
+  db.Place.find()
     .then((places) => {
-      res.render('places/index', { places })
+      res.render("places/index", { places });
     })
-    .catch(err => {
-      console.log(err) 
-      res.render('error404')
-    })
-})
+    .catch((err) => {
+      console.log(err);
+      res.render("error404");
+    });
+});
 
-
-
-router.get('/', (req, res) => {
-  res.send('GET /places stub')
-})
-
-router.post('/', (req, res) => {
+//CREATE
+router.post("/", (req, res) => {
   db.Place.create(req.body)
-  .then(() => {
-      res.redirect('/places')
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
-})
+    .then(() => {
+      res.redirect("/places");
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+});
 
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
 
-
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
-
-router.get('/:id', (req, res) => {
+//UPDATE
+router.get("/:id", (req, res) => {
   db.Place.findById(req.params.id)
-  .then(place => {
-      res.render('places/show', { place })
+    .then((place) => {
+      res.render("places/show", { place });
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+});
+
+router.put("/:id", (req, res) => {
+  console.log(req.params.id);
+  db.Place.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(
+    () => {
+      res.redirect(`/places/${req.params.id}`);
+    }
+  );
+});
+
+//edit
+router
+  .get("/:id/edit", (req, res) => {
+    db.Place.findById(req.params.id).then((place) => {
+      res.render("places/edit", { place });
+    });
+
+    //res.send('GET edit form stub')
   })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
-})
 
+  //DELETE
+  .delete("/:id", (req, res) => {
+    db.Place,
+      findByIdAndDelete(req.params.id).then(() => {
+        res.redirect("/places");
+      });
+  });
 
-router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
-})
+//RANTS
 
-router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
-})
+// router.post('/:id/rant', (req, res) => {
+//   res.send('GET /places/:id/rant stub')
+// })
 
-router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
-})
-
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
-})
-
-router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
-})
-
-module.exports = router
-
-
-
-
-router.get('/', (req, res) => {
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/h-thai-ml-tables.png'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/coffee-cat.jpg'
-  }]
-
-
-
-  
-    res.render('places/index', { places })
-})
-
-
-
-
-router.post('/', (req, res) => {
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
-})
-
-router.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-      // Dig into req.body and make sure data is valid
-      if (!req.body.pic) {
-          // Default image if one is not provided
-          req.body.pic = 'http://placekitten.com/400/400'
-      }
-      if (!req.body.city) {
-          req.body.city = 'Anytown'
-      }
-      if (!req.body.state) {
-          req.body.state = 'USA'
-      }
-
-      // Save the new data into places[id]
-      places[id] = req.body
-      res.redirect(`/places/${id}`)
-  }
-})
-
-
-
-
-
+// router.delete('/:id/rant/:rantId', (req, res) => {
+//     res.send('GET /places/:id/rant/:rantId stub')
+// })
 
 module.exports = router;
-
